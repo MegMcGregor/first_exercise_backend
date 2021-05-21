1cb2862892a0a9ddbb4e3c045b223b7b6851a384
Meg McGregor <megmcgregor1@gmail.com>
2021-05-21 11:36:09 -0700

    first day exercises

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+Console.Write("First Name> ");
+string firstName = Console.ReadLine();
+
+Console.Write("Last Name> ");
+string lastName = Console.ReadLine();
+
+Console.WriteLine($"{lastName} {firstName}");
+
+string reverse = string.Empty;
+string fullName = firstName + " " +  lastName;
+
+for (int i = fullName.Length - 1; i >= 0; i--) 
+{
+    reverse += fullName[i];
+}
+
+Console.WriteLine(reverse);
+
+
+
+
+// string initials = firstName[0] + " " + lastName[0];
+// Console.WriteLine(initials);
+
+// for (int num = 10; num >= 1; num--)
+//     {
+//         Console.Write(num);
+//     }
+
+
+// System.Console.WriteLine("Hello World!");
+
+// System.Console.Write("Who would you like to say hello to? ");
+
+// string name = System.Console.ReadLine();
+
+// if (string.IsNullOrWhiteSpace(name))
+// {
+//     System.Console.WriteLine("Fine, don't say 'hello'!");
+// }
+// else
+// {
+//     System.Console.WriteLine($"Hello, {name}!");
+// }
